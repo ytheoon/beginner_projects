321fe8bd4cf5373ac4d29575ae3a123e3317d473
jonakes <jonakes@chalmers.se>
2023-05-09 22:36:11 +0200

    pylint problems fixed

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-import  random, re, sys
+#!/usr/bin/env python
+
+"""Simple number guessing game. A random number is generated and you need to guess it! :)"""
+
+import random
+import re
+import sys
+
 
 def get_number():
-    return random.randint(1,100)
+    """Generate a random number
+
+    Returns:
+        int: random integer
+    """
+    return random.randint(1, 100)
+
 
 def get_user_input():
+    """record the user's guess
+
+    Returns:
+        int: user's guess
+    """
     flag = True
     user_input = None
     while flag:
@@ -12,28 +30,44 @@ def get_user_input():
 
         if match_val is None:
             print("Please enter an integer:")
-        elif int(user_input)<1 or int(user_input)>100:
+        elif int(user_input) < 1 or int(user_input) > 100:
             print("Number should be between 1 and 100")
         else:
             flag = False
     return int(user_input)
 
+
 def check_answer(guess, answer):
+    """check whether the guess is correct
+
+    Args:
+        guess (int): the users guess
+        answer (int): the actual answer
+
+    Returns:
+        bool: true if the guess is right
+              false if the guess is wrong
+    """
     if guess < answer:
         print("\nGuess a higher number")
         return False
-    elif guess > answer:
+    if guess > answer:
         print("\nGuess a lower number")
         return False
     else:
         print("You Won!!!")
-        return True        
+        return True
 
-answer = get_number()
-answer_found = False
-while not answer_found:
-    guess = get_user_input()
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    answer_found = check_answer(guess, answer)
 
-sys.exit()
+def run_program():
+    """start the game"""
+    answer = get_number()
+    answer_found = False
+    while not answer_found:
+        guess = get_user_input()
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        answer_found = check_answer(guess, answer)
+    sys.exit()
+
+
+run_program()
